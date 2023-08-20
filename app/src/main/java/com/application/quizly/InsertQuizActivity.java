56539a916a6604b6af4593449bf80f8ace1edd5c
LazyJackyboy <skrok.jacek21@gmail.com>
2023-08-20 16:34:40 +0200

    code refactor, added save menu

--- app/src/main/java/com/application/quizly/InsertQuizActivity.java
+++ app/src/main/java/com/application/quizly/InsertQuizActivity.java
@@ -1,6 +1,22 @@
 package com.application.quizly;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InsertQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_quiz);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
+    }
 }
