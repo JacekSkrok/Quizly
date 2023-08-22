96bf5a871e7620a9a001d502f4ec23e2fcafc247
LazyJackyboy <skrok.jacek21@gmail.com>
2023-08-22 18:28:33 +0200

    Adding creation of insatnce of Database and methods to save and clean quiz

--- app/src/main/java/com/application/quizly/InsertQuizActivity.java
+++ app/src/main/java/com/application/quizly/InsertQuizActivity.java
@@ -3,15 +3,65 @@ package com.application.quizly;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class InsertQuizActivity extends AppCompatActivity {
+
+    private FirebaseDatabase aFirebaseDatabase;
+    private DatabaseReference aDatabaseReference;
+
+    EditText txtTitle;
+    EditText txtCategory;
+    EditText txtDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_quiz);
+
+        aFirebaseDatabase = FirebaseDatabase.getInstance();
+        aDatabaseReference = aFirebaseDatabase.getReference().child("quizgames");
+
+        txtTitle = findViewById(R.id.txtTitle);
+        txtCategory = findViewById(R.id.txtCategory);
+        txtDescription = findViewById(R.id.txtDescription);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_menu:
+                saveQuiz();
+                Toast.makeText(this, "Quiz was saved",Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void saveQuiz() {
+        String title = txtTitle.getText().toString();
+        String category = txtCategory.getText().toString();
+        String description = txtDescription.getText().toString();
+        Quiz quiz = new Quiz(title, category, description, "");
+        aDatabaseReference.push().setValue(quiz);
+    }
+
+    private void clean() {
+        txtTitle.setText("");
+        txtDescription.setText("");
+        txtCategory.setText("");
+        txtTitle.requestFocus();
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
